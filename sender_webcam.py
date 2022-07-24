f245bca94381f2851dd63281a6df235abad9fd0f
Rafael <rafaelchavesaec@gmail.com>
2022-07-24 16:05:31 -0300

    Atualização do repositório

--- sender_webcam.py
+++ sender_webcam.py
@@ -2,13 +2,13 @@ import serial
 import hashlib
 import cv2
 
-ser = serial.Serial('/dev/ttyUSB0', 115200)
-cam_port = 0
-cam = cv2.VideoCapture(cam_port)
-result, image = cam.read()
-if result:
-    result, image = cv2.imencode('.jpg', image)
+with serial.Serial('/dev/ttyUSB0', 115200) as ser: 
+    cam_port = 0
+    cam = cv2.VideoCapture(cam_port)
+    result, image = cam.read()
     if result:
-        img_bytes = image.tobytes()
-        ser.write(img_bytes)    
-print(hashlib.sha224(img_bytes).hexdigest(), len(img_bytes))
+        result, image = cv2.imencode('.jpg', image)
+        if result:
+            img_bytes = image.tobytes()
+            ser.write(img_bytes)    
+    print(hashlib.sha224(img_bytes).hexdigest(), len(img_bytes))
